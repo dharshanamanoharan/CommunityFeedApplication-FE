368567cd7297dfee9d85a0d2fee781fd701c7172
dharshanamanoharan <dharshana13@gmail.com>
2024-05-13 22:31:45 +0530

    modified

---
+++ src/Components/Header.js
@@ -0,0 +1,49 @@
+
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { isAuthenticated } from "./AuthenticationFunctions";
+import { useEffect } from "react";
+const Header=()=>{
+    const location1=useLocation();
+    console.log("location",location1);
+    console.log("check authenticated1",isAuthenticated())
+    console.log("check authenticated2",localStorage.getItem('authenticatedUser'))
+    const navigator=useNavigate();
+    const handleLogout=()=>{
+        navigator("/");
+        localStorage.clear();
+        window.location.reload(true);
+    }
+    
+    return(
+        <section className="feed-header">
+            <div className="row feed-navbar sticky-top px-0 mx-0 py-3">
+                <div className="col-1 text-start">
+                 {((location1.pathname).includes("/feed/register") || (location1.pathname).includes("/feed/profile"))
+                    ? <button onClick={()=>navigator("/")}>
+                        <i class="fa-solid fa-arrow-left"></i>
+                      </button>
+                    :<button data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">
+                        <i className="fa-solid fa-bars"></i>
+                    </button>}
+                </div>
+                <div className="col-11 text-center">
+                    <h1>Feeds Application</h1>
+                </div>
+            </div>
+            <div className="offcanvas offcanvas-start py-5" tabindex="-1" id="offcanvasExample" >
+                <div className="offcanvas-body">
+                    <ul>
+                        <>
+                            <li><Link to="/feed"><i className="me-2 fa-solid fa-house"></i>Home</Link></li>
+                            <li><Link to="/"><i className="me-2 fa-solid fa-power-off"></i>Login</Link></li> 
+                        {(isAuthenticated()) ? <li><Link to="/" onClick={handleLogout}><i className="me-2 fa-solid fa-power-off"></i>Logout</Link></li>:<></>}
+                        </>
+            
+                    </ul>
+                
+                </div>
+            </div>
+        </section>
+    );
+};
+export default Header;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,69 @@
-const Login=()=>{
-    return(
-        <>
-            
-        </>
-    )
 
+import { showPassword1} from "./AuthenticationFunctions";
+import { useState } from "react";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
+const Login=()=>{
+    const navigator=useNavigate();
+    const [userData,setUserData]=useState();
+    //Login field Values
+    const [userName,setUserName]=useState("");
+    const [password,setPassword]=useState("");
+
+    //To Display Error
+    const [err1,setErr1]=useState("");
+    const [err2,setErr2]=useState("");
+    const [loginMsg,setLoginMsg]=useState("");
+
+    
+    //To Clear
+    const regClear=()=>{
+        document.getElementById('floatingInput1').value="";
+        document.getElementById('floatingPassword1').value="";
+        setEmail("");setPassword("");
+    }
+    const handleLogin=async()=>{
+        setErr1(""); setErr2("") ;setLoginMsg("");
+        var flag1,flag2;
+     
+        (userName.trim()==="")?setErr1("Enter valid user name!"):flag1=true;
+        (password.trim()==="")?setErr2("Enter the password!"):flag2=true; 
+       
+        if(flag1===true && flag2===true)
+        {
+            try{
+               const res1=await axios.post("http://localhost:8080/feed/login",{userName,password});
+               const res2=await axios.get("http://localhost:8080/feed/user/"+userName);
+               setUserData(res2.data);
+               localStorage.setItem("userId1",res2.data.userId);
+               localStorage.setItem("authenticatedUser",userName);
+               navigator("/feed");
+               setLoginMsg("");
+            }
+            catch(error)
+            {
+                setLoginMsg("Login Failed!");
+                console.log("Login Failed",error);
+            }
+        }
+    }
+    return(
+       <section className="container-fluid todo-login py-5">
+            <div className="form-floating ">
+                <input type="text" className="form-control" id="floatingInput1" placeholder="" onChange={(e)=>setUserName(e.target.value)}/>
+                <label for="floatingInput1">UserName</label>
+            </div>
+            <p className="regErr">{err1}</p>
+            <div className="form-floating  pwd-visibility">
+                <input type="password" className="form-control" id="floatingPassword1" placeholder="" onChange={(e)=>setPassword(e.target.value)}/>
+                <label for="floatingPassword1">Password</label>
+                <i className="fa-regular fa-eye-slash" id="icon1" onClick={showPassword1}></i>
+            </div>
+            <p className="regErr">{err2}</p>
+                <button className="m-3" onClick={handleLogin}>Login</button>
+            <p className="regErr" style={{color:(loginMsg.includes("Success"))?"green":"red"}}>{loginMsg}</p>
+            <p className="reg-now">Don't have an account?<Link to="/feed/register" >Register now</Link></p>
+       </section>
+    );
 }
 export default Login;
