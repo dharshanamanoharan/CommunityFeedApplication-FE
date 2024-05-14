fbd7b35eae4f3ca88a0331b383022adbc2e81555
dharshanamanoharan <dharshana13@gmail.com>
2024-05-14 10:08:00 +0530

    modified

--- src/Components/Feeds.js
+++ src/Components/Feeds.js
@@ -1,7 +1,15 @@
 const Feeds=()=>{
     return(
-        <>
-        </>
+        <section className="feed-section container-fluid">
+            <div className="row">
+                <textarea id="feed-post"></textarea>
+                <button className="create-post">Create Post</button>
+            </div>
+            <div className="row">
+                <button className="view-my-post">View my Post</button>
+                <button className="view-my-post">View other Post</button>
+            </div>
+        </section>
     )
 
 }
